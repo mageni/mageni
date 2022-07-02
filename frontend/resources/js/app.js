dc739ba84bd1f55e008a43c6adc89310a2a2870a
Jonathan Jaquez <jejaquez@mageni.net>
2022-07-02 07:21:34 +0000

    Added confetti notifications

--- frontend/resources/js/app.js
+++ frontend/resources/js/app.js
@@ -2,8 +2,18 @@ require('./bootstrap');
 
 import Alpine from 'alpinejs';
 import Swal from 'sweetalert2';
+import confetti from 'canvas-confetti';
 
 window.Alpine = Alpine;
 window.Swal = Swal;
+
+
+Livewire.on('confetti', () => {
+    confetti({
+        particleCount: 80,
+        spread: 200,
+        origin: {y: 0.6}
+    });
+})
 
 Alpine.start();
