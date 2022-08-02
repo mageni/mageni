e293fc12add6f1aa6304e120de4c593d743c6169
Jonathan Jaquez <jejaquez@mageni.net>
2022-08-02 17:39:59 +0000

    Update About Page to Version 1.2.1

--- frontend/resources/views/livewire/about.blade.php
+++ frontend/resources/views/livewire/about.blade.php
@@ -17,7 +17,7 @@
                         </div>
                         <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Version</dt>
-                            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">1.1.11</dd>
+                            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">1.2.1</dd>
                         </div>
                         <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">About</dt>
