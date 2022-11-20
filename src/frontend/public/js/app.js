c5f9baa0b367c12a2b561a915fce5c1843674484
Shift <shift@laravelshift.com>
2022-11-20 17:17:57 +0000

    Rename ENV variables

--- src/frontend/public/js/app.js
+++ src/frontend/public/js/app.js
@@ -5075,8 +5075,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // window.Pusher = require('pusher-js');
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
 
